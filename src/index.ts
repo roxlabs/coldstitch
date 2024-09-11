41955ede1e78638a897579882ea16b2ad27d0a13
Daniel Rochetti <daniel.rochetti@gmail.com>
2024-09-11 02:23:06 -0700

    feat: export some utils (#7)

--- src/index.ts
+++ src/index.ts
@@ -1,2 +1,3 @@
 export { code, omitLine, type Code } from "./code";
 export { isTypeRef, type ImportResolver, type TypeRef } from "./types";
+export { escapeStringQuotes, isPlainObject, isScalar, stringify } from "./utils";
